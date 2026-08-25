1a7e1e4db1d1c38c5d01b9b7a5055583533b153c
Gada-Alsalem <gadaalsalem@yahoo.com>
2026-08-25 14:55:19 +0300

    Update create_table.sql

--- Marketing Campaign Dashboard/sql_queries/create_table.sql
+++ Marketing Campaign Dashboard/sql_queries/create_table.sql
@@ -11,6 +11,8 @@ CREATE TABLE daily_campaign_performance (date date,
 -- confirm csv was imported --
 SELECT * FROM daily_campaign_performance;
 
+
+
 CREATE TABLE campaigns (campaign_id text,
 						campaign_name text,
 						channel text,
@@ -20,3 +22,13 @@ CREATE TABLE campaigns (campaign_id text,
 						planned_budget integer);
 
 SELECT * FROM campaigns;
+
+
+
+CREATE TABLE customer_orders (order_id text,
+								customer_id text,
+								order_date date,
+								acquisition_channel text,
+								order_amount numeric);
+
+SELECT * FROM customer_orders;
